b3f2b02ba2c97de4d12667390eab1cf3b31a0f79
mvtsytovych <m.v.tsytovych@student.khai.edu>
2025-10-06 11:05:53 +0300

    Create main.cpp

---
+++ fold/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>// підключення бібліотеки математичних функцій
+using namespace std;
+//28, 16, 7
+int main() {
+	int x, y, eq;
+	cin >> y, x;
+
+	eq = 3 * x + 2;
+
+	if (y > eq) {
+		cout << "вище прямої";
+	}
+	else if (eq < x)
+	{
+		cout << "нище прямої";
+	}
+	else {
+		cout << "на прямії";
+	}
+	
+}
