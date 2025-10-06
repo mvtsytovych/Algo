b82fe5d427d2a465754779769de5b3f61f4c6746
mvtsytovych <m.v.tsytovych@student.khai.edu>
2025-10-06 11:39:33 +0300

    Update main.cpp

--- fold/main.cpp
+++ fold/main.cpp
@@ -4,19 +4,20 @@ using namespace std;
 
 int main() {
 	int y, x, eq;
-		cout << "Введіть y,x\n";
-		cin >> y >> x;
+	cout << "Функція y = 3x + 2\n";
+	cout << "Введіть y, x\n";
+	cin >> y >> x;
 
-		eq = 3 * x + 2;
-
-		if (y > eq) {
-			cout << "вище лінії";
-		}
-		else if (y < eq)
-		{
-			cout << "нижче лінії";
-		}
-		else {
-			cout << "На лінії";
-		}
+	eq = 3 * x + 2;
+	
+	if (y > eq) {
+		cout << "вище лінії";
+	}
+	else if (y < eq)
+	{
+		cout << "нижче лінії";
+	}
+	else {
+		cout << "На лінії";
+	}
 }
