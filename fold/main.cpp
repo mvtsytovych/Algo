93db96101e8eda48327b63314f086c1cc7ae53a6
mvtsytovych <m.v.tsytovych@student.khai.edu>
2025-10-06 11:13:08 +0300

    Update main.cpp

--- fold/main.cpp
+++ fold/main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main() {
 	int x, y, eq;
+	cout << "Введіть y, x\n";
 	cin >> y>> x;
 
 	eq = 3 * x + 2;
@@ -11,7 +12,7 @@ int main() {
 	if (y > eq) {
 		cout << "вище прямої";
 	}
-	else if (eq < x)
+	else if (y < eq)
 	{
 		cout << "нище прямої";
 	}
